d431cf4df51c87e45937724b7836ebcafc1b6bb9
samynjonas <samynjonas@live.be>
2022-04-21 17:21:48 +0200

    Enemy

--- StickmanTeamTowerOfDoom/Enemy.cpp
+++ StickmanTeamTowerOfDoom/Enemy.cpp
@@ -26,3 +26,18 @@ Enemy::TypeOfEnemy Enemy::GetTypeOfEnemy(){return m_EnemyType;}
 void Enemy::Movement(float elapsedSec)
 {
 }
+
+OAMEntry Enemy::sendOAM()
+{
+	//int posX, int posY, int tileID, bool flipX, bool flipY
+	int posX{ m_ActorRect.left };
+	int posY{ m_ActorRect.bottom };
+	int TileID{ 10 };
+	bool flipX{ false };
+	bool flipY{ false };
+
+	OAMEntry entry = OAMEntry{ posX, posY, TileID, flipX, flipY };
+
+	return entry;
+}
+
--- StickmanTeamTowerOfDoom/Enemy.h
+++ StickmanTeamTowerOfDoom/Enemy.h
@@ -22,6 +22,8 @@ public:
 	virtual void Update(float elapsedSec) override;
 	TypeOfEnemy GetTypeOfEnemy();
 
+	virtual OAMEntry sendOAM() override;
+
 protected:
 	virtual void Movement(float elapsedSec);
 

4c1a211fb9f40488720847d7f175c6fef9158efe
Wardergrip <42802496+Wardergrip@users.noreply.github.com>
2022-04-21 14:29:48 +0200

    Added Projectile class

---
+++ StickmanTeamTowerOfDoom/Projectile.cpp
@@ -0,0 +1,50 @@
+#include "pch.h"
+#include "Projectile.h"
+#include "utils.h"
+using namespace utils;
+
+Projectile::Projectile(Type type, const Point2f& pos, int damage)
+	:m_Type(type)
+	,m_TimeAlive()
+	,m_Rect()
+	,m_Velocity(100,0)
+	,m_Damage(damage)
+{
+	m_TimeAlive.setTimeGoal(16.f);
+}
+
+Projectile::~Projectile()
+{
+}
+
+bool Projectile::IsOverlapping(const Rectf& other)
+{
+	return utils::IsOverlapping(m_Rect,other);
+}
+
+void Projectile::SetDamage(int dmg)
+{
+	if (dmg < 0) return;
+	m_Damage = dmg;
+}
+
+void Projectile::SetVelocity(const Vector2f& velocity)
+{
+	m_Velocity = velocity;
+}
+
+void Projectile::SetHorVelocity(float xVel)
+{
+	m_Velocity.x = xVel;
+}
+
+void Projectile::SetPosition(const Point2f& pos)
+{
+	m_Rect.left = pos.x;
+	m_Rect.bottom = pos.y;
+}
+
+int Projectile::GetDamage() const
+{
+	return m_Damage;
+}
---
+++ StickmanTeamTowerOfDoom/Projectile.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "Timer.h"
+class Projectile
+{
+public:
+	enum class Type
+	{
+		bullet,boomerang,laser
+	};
+
+	explicit Projectile(Type type = Type::bullet, const Point2f& pos = {}, int damage = 10);
+	Projectile(Projectile& p) = delete;
+	Projectile& operator=(Projectile& p) = delete;
+	Projectile(Projectile&& p) = delete;
+	Projectile& operator=(Projectile&& p) = delete;
+	virtual ~Projectile();
+
+	// See if a rect is overlapping with the internal rect
+	bool IsOverlapping(const Rectf& other);
+	virtual void Update(float elapsedSec) = 0;
+
+	void SetDamage(int dmg);
+	void SetVelocity(const Vector2f& velocity);
+	// Changes internal velocity's x component
+	void SetHorVelocity(float xVel);
+	// Changes bottom left of internal rect
+	void SetPosition(const Point2f& pos);
+
+	int GetDamage() const;
+protected:
+	Type m_Type;
+	Timer m_TimeAlive;
+	Rectf m_Rect;
+
+	Vector2f m_Velocity;
+
+	int m_Damage;
+};
+
